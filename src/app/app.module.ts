a7269f77307bd2c7f7afa01453c14718be231b81
Thiago <Thiago_gadelha95@hotmail.com>
2023-10-22 19:06:13 -0300

    style: navbar

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavebarComponent } from './navebar/navebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
